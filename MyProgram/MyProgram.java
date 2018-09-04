a7e6ff57fbf02e6348b21101ff6cd40407686f1f
Suresh Dhaka <choudhary1.dhaka@gmail.com>
2018-09-04 22:10:17 +1200

    MyProgram .... मेरे द्वारा बनाई गई सरल सॉफ्टवेयर , simple software created by me

---
+++ MyProgram/MyProgram.java
@@ -0,0 +1,8 @@
+public class MyProgram{
+	
+	public static void main(String str []){
+		System.out.println("********************Start*************************");
+		System.out.println("Hello Guys, This is Also a PROGRAM!");
+		System.out.println("********************END*************************");
+	}
+}
